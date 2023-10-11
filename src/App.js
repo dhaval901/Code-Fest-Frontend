5c2677845fefd4b60baaa529b8dee7e493da7151
sonali <wankhade.sonali@gmail.com>
2023-10-11 21:29:55 +0530

    Initial commit, blank page with Azure chatbot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Settings } from "@mui/icons-material";
+import { Button, Typography } from "@mui/material";
+import styled from "styled-components";
+import Chatbot from "./Chatbot";
 
 function App() {
+  const BlueButton = styled(Button)({
+    backgroundColor: "skyblue",
+    color: "#345",
+    margin: 5,
+    "&:hover": { backgroundColor: "lightblue" },
+    "&:disbaled": { backgroundColor: "gray", color: "white" },
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacting
-        </a>
-      </header>
+      {/* <Button variant="text" startIcon={<Settings />}>
+        Text
+      </Button>
+      <Button variant="contained" color="otherColor">
+        Contained
+      </Button>
+      <Button variant="outlined">Outlined</Button> */}
+      <Typography variant="h4" component="p">
+        This is H4 type with P component
+      </Typography>
+      <Chatbot/>
+      {/* <BlueButton>Another Button</BlueButton>
+      <BlueButton>My Button</BlueButton> */}
     </div>
   );
 }
