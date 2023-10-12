72358166da223763799073fc6a1f0ad79d8951ae
sonali <wankhade.sonali@gmail.com>
2023-10-12 13:58:16 +0530

    Added chatbot

---
+++ src/chatbot.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chatbot = () => {
+  return (
+    <div>
+      <iframe src="https://web.powerva.microsoft.com/environments/Default-9ff87383-ae93-4e4b-85fd-e21685c0431c/bots/crae2_listeningear/webchat?__version__=2" ></iframe>
+    </div>
+  )
+}
+
+export default Chatbot
