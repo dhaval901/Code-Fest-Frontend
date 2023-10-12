7ddd26a91b51ecf92f06a4d1706a047b2e7ac7db
shishirvyas <shishir.svyas@gmail.com>
2023-10-12 10:55:35 +0530

    test

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+module.exports = {
+    preset: 'ts-jest',
+    moduleFileExtensions: ['ts', 'tsx', 'js'],
+    globals: { 'ts-jest': { tsConfig: 'tsconfig.json' } },
+    testMatch: ['**/test/**/*.test.(ts|tsx)'],
+    // ...
+    passWithNoTests: true,
+};
+
